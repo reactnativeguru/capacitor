3501a769f0715ab95bea932bd97a5be1bb5d462f
“John” <john@reactnative.guru>
2024-02-12 08:47:49 +0000

    base app structure

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Invoices from './routes/Invoices';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />}>
+					<Route path="/invoices" element={<Invoices />} />
+					<Route
+						path="*"
+						element={
+							<main style={{ padding: '1rem' }}>
+								<p>There's nothing here!</p>
+								<Link to="/">Back home!</Link>
+							</main>
+						}
+					/>
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
